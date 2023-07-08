f84264f1eb96df2bb350ac606fe1092975e61de7
liliantbo <liliantbo@gmail.com>
2023-07-08 04:24:30 -0500

    estadisticas

--- src/Admin/BarChart.js
+++ src/Admin/BarChart.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
+import { useSelector } from 'react-redux';
+import { STATE_CANCELED, STATE_DELIVERED, STATE_DISPATCH, STATE_RETURNED, STATE_SENDED } from '../commonComponents/Properties';
 
 const BarChart = () => {
+  //redux store
+  const albumList = useSelector(state => state.alb.albumList);
+
+  // Obtener la fecha actual
+  const today = new Date();
+
+  // Obtener la fecha de hace 30 días
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(today.getDate() - 30);
+
+  // Filtrar los álbumes por fecha dentro del rango de los últimos 30 días
+  const filteredAlbums = albumList.filter((album) => {
+    const albumDate = new Date(album.fecha);
+    return albumDate >= thirtyDaysAgo && albumDate <= today;
+  });
+
+  const albumCountByState = filteredAlbums.reduce((count, album) => {
+    const { estado } = album;
+    if (count.hasOwnProperty(estado)) {
+      count[estado]++;
+    } else {
+      count[estado] = 1;
+    }
+    return count;
+  }, {});
+
+
   const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    labels: [STATE_SENDED, STATE_DISPATCH, STATE_DELIVERED, STATE_RETURNED, STATE_CANCELED],
     datasets: [
       {
-        label: 'Sales',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Ordenes',
+        data: [albumCountByState[STATE_SENDED],
+        albumCountByState[STATE_DISPATCH],
+        albumCountByState[STATE_DELIVERED],
+        albumCountByState[STATE_RETURNED],
+        albumCountByState[STATE_CANCELED]],
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1,
@@ -27,7 +60,7 @@ const BarChart = () => {
 
   return (
     <div>
-      <h2>Bar Chart</h2>
+      <h2>Ultimos 30 días</h2>
       <Bar data={data} options={options} />
     </div>
   );
--- src/Admin/Statistics.js
+++ src/Admin/Statistics.js
@@ -2,7 +2,7 @@ import BarChart from "./BarChart";
 export default function AlbumMakerAdmin() {
     return (
         <div>
-      <h1>My Chart App</h1>
+      <h1>Ordenes por Estado</h1>
       <BarChart />
     </div>
     );
